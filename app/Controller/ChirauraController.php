acb442046b91c0d35baba6dfb9fa93a689853452
yamamoto <yamamoto@s14.yeskist.net>
2015-12-11 15:16:47 +0900

    no message

---
+++ app/Controller/ChirauraController.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+class ChirauraController extends AppController {
+
+  public $name = 'Chiraura';
+  public $user = null;
+  public $layout = "Chiraura";
+  public $autoRender = true;
+
+  public function index(){
+	$title_for_layout = "チラシの裏";
+	$this->set(compact('title_for_layout'));
+	
+/*	$data = $this->Chiraura->find('all');
+	$this->set('data', $data);*/
+  }
+  
+  public function other(){
+	echo "これは、index以外のページです。";
+  }
+}
+?>
